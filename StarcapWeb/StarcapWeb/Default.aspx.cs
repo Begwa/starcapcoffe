1cfbb5fc911592e0b3231a85c736da12b67a9eeb
Bernardo Segovia <zeb3xy@hotmail.com>
2021-07-14 12:17:18 -0400

    Generar interfaz, Generar cliente

---
+++ StarcapWeb/StarcapWeb/Default.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace StarcapWeb
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string[] cafes = { "Moca", "Capuccino", "Vainilla", "Expreso", "Americano"};
+            CafeDdl.DataSource = cafes;
+            CafeDdl.DataBind();
+        }
+    }
+}
